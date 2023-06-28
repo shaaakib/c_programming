4a9531b5ebf15181ae4393ccb113a4d8d90869bd
Muhammad Shakib Hossain <>
2023-06-28 12:31:05 +0600

    leap year

--- if_else/divisibleby5.c
+++ if_else/divisibleby5.c
@@ -4,9 +4,9 @@ int main() {
     printf("Enter a number : ");
     scanf("%d", &n);
     if(n%5 == 0){
-        printf("");
+        printf("Divisible by 5");
     }else{
-        printf("");
+        printf("Not divisible by 5");
     }
     return 0;
 }
