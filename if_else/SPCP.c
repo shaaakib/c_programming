cdc75d2b234fd76675f853f527eee0c66e1f2bfe
Muhammad Shakib Hossain <>
2023-06-28 14:06:25 +0600

    Sp Cp

---
+++ if_else/SPCP.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () {
+    int cp;
+    printf("Enter cost price : ");
+    scanf("%d", &cp);
+    int sp;
+    printf("Enter selling price : ");
+    scanf("%d", &sp);
+    if(sp>cp){
+        printf("Profit!");
+    }
+    if(cp>sp){
+        printf("Loss!");
+    }if(sp == cp){
+        printf("No profit, no Loss!");
+    }
+    return 0;
+}
