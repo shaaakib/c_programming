b99b6ae29bbd4ce68709081619b4b24e178adb8f
Muhammad Shakib Hossain <>
2023-06-29 14:48:49 +0600

    predict

---
+++ if_else/predict4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int x = 3;
+    float y = 3.0;
+    if(x==3)
+        printf("\n x and t are equal");
+    else
+        printf("\n x and y are not equal");
+    return 0;
+}
