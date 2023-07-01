2b3b21a489fbd049f2fdfb27000ac262fbf818e2
Muhammad Shakib Hossain <>
2023-07-01 14:49:27 +0600

    apodd

---
+++ loops/APOdd.c
@@ -0,0 +1,11 @@
+// ? Ques : Display this AP - 1,3,5,7,9.. upto ‘n’ terms.
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    for(int i=1; i<=2*n-1; i =i+2){
+        printf("%d ", i);
+    }
+    return 0;
+}
