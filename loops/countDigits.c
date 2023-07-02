50941e980b149109bf705ca0fbdfff2ae3f793e0
Muhammad Shakib Hossain <>
2023-07-02 21:35:13 +0600

    sum digits

---
+++ loops/countDigits.c
@@ -0,0 +1,14 @@
+// ? Ques : WAP to count digits of a given number.
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    int count = 0;
+    while(n!=0){
+        n = n/10;
+        count++;
+    }
+    printf("The no of digits are %d", count);
+    return 0;
+}
