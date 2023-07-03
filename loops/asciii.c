f6f259aaf3ce2dd72052a1c6dfd10f10709d1caa
Muhammad Shakib Hossain <>
2023-07-03 12:14:31 +0600

    ascii

---
+++ loops/asciii.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    // what is typecasting
+    // int x = 65;
+    // printf("%d\n",x);
+    // char ch = (char)x;
+    // printf("%c\n",ch);
+
+    for(int i =65; i<=90; i++){
+        printf("%d ", i);
+        char ch = (char)i;
+        printf("%c\n",ch);
+    }
+    return 0;
+}
