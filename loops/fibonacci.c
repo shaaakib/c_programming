be2c909fff86e22abf1edcf5a04e78de5214dacf
Muhammad Shakib Hossain <>
2023-07-03 11:48:06 +0600

    fibonacci

---
+++ loops/fibonacci.c
@@ -0,0 +1,17 @@
+// ? Ques : Print the nth fibonacci number.
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    int a = 1;
+    int b = 1;
+    int sum = 1;
+   for(int i = 1; i<=n-2; i++){
+    sum = a + b;
+    a = b;
+    b = sum;
+   } 
+   printf("The %dth fibonacci of is %d",n, sum);
+    return 0;
+}
