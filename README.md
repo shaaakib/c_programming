dc21c40800d62cf9863338f2378f21c2b424fa37
Muhammad Shakib Hossain <>
2023-07-09 06:51:09 +0600

    basic array

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - C in One Shot Part - 1 basic : https://drive.google.com/file/d/1h-jkmy-fobkXRBr5XFCZXctneNx9EDtX/view
 - C in One Shot Part - 2 if else : https://drive.google.com/file/d/1N1ahGfZc4wKOIysFsJjymBlMtodpuhen/view
 - C in One Shot Part - 3 loops : https://drive.google.com/drive/folders/1-4tUD_2wNBM7QPSyY36Ev9gkcghePTQD
+
+- C in One Shot Part - 3 array : https://drive.google.com/drive/folders/1-4tUD_2wNBM7QPSyY36Ev9gkcghePTQD
---
+++ array/basicArray.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+    int  main() {
+        // int arr[5] = {2, 4, 6, 8, 1};
+        // arr[4] = 100; // {2, 4, 6, 100}
+        // arr[1] = 1;
+        // int arr[4];
+        // printf("Enter first element : ");
+        // scanf("%d", &arr[0]);
+        // printf("Enter 2nd element : ");
+        // scanf("%d", &arr[1]);
+        // printf("Enter 3rd element : ");
+        // scanf("%d", &arr[3]);
+        // printf("Enter fourth element : ");
+        // scanf("%d", &arr[4]);
+
+        // printf("%d",arr[0]);
+        // float a[3] = {1.2, 3.4, 5.7};
+        // printf("%f", a[2]);
+        // char arr[4] = {'a', 'n', 'Y', '%'};
+        // printf("%c", arr[3]);
+
+
+        // int arr[5] = {2, 4, 6, 1};
+
+        // for(int i=0; i<4; i++){
+        //     printf("%d ", arr[i]);
+        // }
+
+
+        int arr[5];
+
+        for(int i=0; i<=4; i++){
+            printf("\nEnter element number %d ", i + 1);
+            scanf("%d", &arr[i]);
+        }
+
+        for(int i=0; i<5; i++){
+            printf("%d ", arr[i]);
+        }
+
+        // printf("%d", arr[1]);
+
+        return 0;
+    }
